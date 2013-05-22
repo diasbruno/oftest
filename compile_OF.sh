de0124a2e8a1e4a152bd84584ec79d77741a2589
Bruno Dias <contact@diasbruno.com.br>
2013-05-22 09:24:23 -0300

    improved compile_OF.sh. install dependencies and compile OF outsite of rake.

--- compile_OF.sh
+++ compile_OF.sh
@@ -9,7 +9,10 @@ if [ $EUID != 0 ]; then
    exit 1
 fi
 
-apt-get update
+APT_GET=$(hash apt-get &> /dev/null; echo $?)
+if [ "$APT_GET" -eq "0" ]; then
+    apt-get update
+fi
 
 GSTREAMER_VERSION=0.10
 GSTREAMER_FFMPEG=gstreamer${GSTREAMER_VERSION}-ffmpeg
@@ -23,8 +26,10 @@ if [ "$RET" -eq "0" ]; then
 fi
 
 
-apt-get install freeglut3-dev libasound2-dev libxmu-dev libxxf86vm-dev g++ libgl1-mesa-dev libglu1-mesa-dev libraw1394-dev libudev-dev libdrm-dev libglew-dev libopenal-dev libsndfile-dev libfreeimage-dev libcairo2-dev libgtk2.0-dev python-lxml python-argparse libfreetype6-dev portaudio19-dev libssl-dev
+if [ "$APT_GET" -eq "0" ]; then
+	apt-get install freeglut3-dev libasound2-dev libxmu-dev libxxf86vm-dev g++ libgl1-mesa-dev libglu1-mesa-dev libraw1394-dev libudev-dev libdrm-dev libglew-dev libopenal-dev libsndfile-dev libfreeimage-dev libcairo2-dev libgtk2.0-dev python-lxml python-argparse libfreetype6-dev portaudio19-dev libssl-dev
 apt-get install libgstreamer${GSTREAMER_VERSION}-dev libgstreamer-plugins-base${GSTREAMER_VERSION}-dev  ${GSTREAMER_FFMPEG} gstreamer${GSTREAMER_VERSION}-pulseaudio gstreamer${GSTREAMER_VERSION}-x gstreamer${GSTREAMER_VERSION}-plugins-bad gstreamer${GSTREAMER_VERSION}-alsa gstreamer${GSTREAMER_VERSION}-plugins-base gstreamer${GSTREAMER_VERSION}-plugins-good
+fi
 exit_code=$?
 if [ $exit_code != 0 ]; then
 	echo "error installing packages, there could be an error with your internet connection"
