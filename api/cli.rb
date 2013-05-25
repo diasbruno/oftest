1e08815b56650b882159c50979438789388648d0
Bruno Dias <contact@diasbruno.com.br>
2013-05-25 17:33:37 -0300

    Patch from linux.

--- api/cli.rb
+++ api/cli.rb
@@ -8,6 +8,10 @@ require './api/utils'
 #
 PYGMENTS=system "which pygmentize &> /dev/null"
 
+# Log.
+#
+TESTER_LOG="tester ::"
+
 # Select a random header 
 # from openFrameworks.
 #
@@ -19,20 +23,20 @@ def command_select_random()
     puts find_hs[ select_h ].to_s.white.bold
 end
 
-# Select a random header 
-# from openFrameworks.
+# Create a new test.
 #
 def command_create_test( name )
     template = %x[ cat ./api/templates/test.cpp ]    
-    # puts system "echo \"#{template.gsub( /\{WHAT\}/, name  )}\" | pygmentize -l cpp"
     puts "\n\
 // both ofMain.h and cpptest.h includes\n\
 // will be removed when we glue."
     puts template.gsub( /\{WHAT\}/, name.red  )
 end
 
+# Find the name in a string.
+#
 def find_test_name( filename )
-    check = /^[^\.+$]\w+_(\w+)\..*/.match( filename ) 
+    check = /^[^\.+$]\w+_(of.*)\..*/.match( filename )
     return check[ 1 ].to_s if check
     return ""
 end
@@ -42,24 +46,31 @@ end
 def command_list_tests()
     tests = Dir.entries( "./src" ) if File.exists?( "./src" )
     tests.each do | t |
-        puts find_test_name( t ).to_s.white.bold if not /^\.+$/.match( t )
+        name = find_test_name( t )
+        puts find_test_name( t ).to_s.white.bold if not name.eql? ""
     end
 end
 
-TESTER_NOTICE="tester ::"
-
+# Create the log message.
+#
 def make_log( type, msg )
     return "#{type} #{msg}"
 end
 
+# Simple log.
+#
 def log( message )
-    return make_log( TESTER_NOTICE.white.bold, message )
+    return make_log( TESTER_LOG.white.bold, message )
 end
 
+# Warning message.
+#
 def warning( message )
-    return make_log( TESTER_NOTICE.yellow.bold, message )
+    return make_log( TESTER_LOG.yellow.bold, message )
 end
 
+# Error log.
+#
 def error( message )
-    return make_log( TESTER_NOTICE.red.bold, message )
+    return make_log( TESTER_LOG.red.bold, message )
 end
