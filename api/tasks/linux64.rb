52641f82d8afb54c9bc7bb9d26b2373823b7dccb
Bruno Dias <contact@diasbruno.com.br>
2013-06-10 22:19:55 -0300

    [fixed] linux64 task.

--- api/tasks/linux64.rb
+++ api/tasks/linux64.rb
@@ -13,7 +13,11 @@ namespace :linux64 do
     # Set up all compiler stuff
     #
     task :setup do
-        CXX = "g++"
+
+        OF = Of::Of
+        TP = Of::Tp
+        OFT = Of::Oft
+
         OTHER_FLAGS = "-march=native -mtune=native -Wall -fexceptions -pthread"
         
         # System check
@@ -21,68 +25,99 @@ namespace :linux64 do
         GTK       = check_pkg_config( "gtk+-2.0" )
         MPG123    = check_pkg_config( "libmpg123" )
         LINUX_ARM = false
-
-        if check_pkg_config( "gstreamer-1.0" )
-            GST_VERSION = "1.0"
-        else
-            GST_VERSION = "0.10"
-        end
+        # Default
+        gst_version = "0.10"
+        # If of can use gst 1.0, use it.
+        gst_version = "1.0" if check_pkg_config( "gstreamer-1.0" )
+        GST_VERSION = gst_version
 
         # Dependencies
         #
         libs = "cairo zlib gstreamer-#{GST_VERSION} gstreamer-app-#{GST_VERSION} gstreamer-video-#{GST_VERSION} gstreamer-base-#{GST_VERSION} libudev freetype2 sndfile openal portaudio-2.0 openssl"
 
-        if not LINUX_ARM
-            libs = "#{libs} gl glu glew"
-        end
+        # if not arm
+        libs = "#{libs} gl glu glew" if not LINUX_ARM
 
-        # conditionally add GTK
-        if GTK
-            libs = "#{libs} gtk+-2.0"
-        end
+        # add GTK if available
+        libs = "#{libs} gtk+-2.0" if GTK
 
-        # conditionally add mpg123
-        if MPG123
-            libs = "#{libs} libmpg123"
-        end
+        # add libmpg123 if available
+        libs = "#{libs} libmpg123" if MPG123
 
         # Define CFLAGS
         #
-        if GTK
-            cflags = "#{cflags} -DOF_USING_GTK"
-        end
+        cflags = "#{cflags} -DOF_USING_GTK" if GTK
+        cflags = "#{cflags} -DOF_USING_MPG123" if MPG123
 
-        if MPG123
-            cflags = "#{cflags} -DOF_USING_MPG123"
-        end
-
+        # make CFLAGS for libs
         pkg_libs = %x[ pkg-config #{libs} --cflags ].strip
         
         cflags = "#{cflags} #{pkg_libs}"
-        THIRDPARTY_HEADERS_MORE = find_tps_expand_path( THIRDPARTY_HEADERS, "-I " )
-        cflags = "#{cflags} #{make_search_path_from_list( THIRDPARTY_HEADERS_MORE, "-I " )}"
-        cflags = "#{cflags} #{make_search_path_from_list( OF_HEADERS, "-I" )}"
+
+        # add openFrameworks includes
+        cflags = "#{cflags} #{fold( "-I ", OF.headers )}"
+
+        # take all tp headers
+        #
+        tp_headers = []
+        TP.libs.each do | lib |
+            # exclude what should not be included
+            # quicktime is not use
+            headers = fold( "-I ", lib.headers ) if not /quicktime/.match( lib.name )
+            # poco we just need poco/include
+            # headers = "-I #{lib.path_include}" if /poco/.match( lib.name )
+            
+            tp_headers.push headers
+        end
+        
+        cflags = "#{cflags} #{tp_headers.join( " " )}"
+        cflags = "#{cflags}"
+
+        # finally push all to CFLAGS const
         CFLAGS = cflags
 
         # Define LDFLAGS
         #
         ldflags = "-march=native -mtune=native"
-        ldflags = "#{ldflags} -Wl,-rpath=./libs"
          
         pkg_libs = %x[ pkg-config #{libs} --libs ].strip
         
+        tp_libs = []
+        tp_search = []
+        TP.libs.each do | lib |
+            
+            # use the bin/libs
+            tp_search.push lib.path_lib if not /fmodex/.match( lib.name )
+
+            # exclude what should not be included
+            # quicktime is not use
+            # poco we just need poco/include
+            libs = fold( " ", lib.libs ) if not /poco|fmodex/.match( lib.name )
+            
+            tp_libs.push libs
+        end
+        
+        # inject fmodex ./bin/libs
+        tp_search.push "./bin/libs"
+
+        tp_libs = tp_libs.delete_if {|i| i.eql? "" }.join( " " )
+        tp_search = fold( "-L ", tp_search.delete_if {|i| i.eql? "" } )
+        
+        ldflags = "#{ldflags} -Wl,-rpath=./libs"
+
+        ldflags = "#{ldflags} -L#{OF.path_lib} -lopenFrameworksDebug"
+        ldflags = "#{ldflags} -L./libs/cpptest/lib/#{OS} -lcpptest"
+
         ldflags = "#{ldflags} #{pkg_libs} -lfreeimage -lfmodex -lFLAC -logg -lglut -lvorbis -ljack"
-        ldflags = "#{ldflags} -L../libs/openFrameworksCompiled/lib/#{OS} -lopenFrameworksDebug"
-        ldflags = "#{ldflags} -L ./libs/cpptest/lib/#{OS} -lcpptest"
-        ldflags = "#{ldflags} #{make_search_path_from_list( THIRDPARTY_LIBS_PATH, "-L " )}"
+        ldflags = "#{ldflags} #{tp_search} #{tp_libs}"
         
         # we need to exlude poco 
-        filtered_libs = exclude( THIRDPARTY_LIBS, /poco|.\.so/ )
-        
-        ldflags = "#{ldflags} #{filtered_libs.join(" ")}"
         ldflags = "#{ldflags} -lPocoCrypto -lPocoNetSSL -lPocoNet -lPocoXML -lPocoFoundation -lPocoUtil"
 
         LDFLAGS = "#{ldflags}"
+
+        puts LDFLAGS
+        # fail "1"
     end
 
 
